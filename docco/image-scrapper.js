61c893bdbe8e1c3b3956aa43c1cd6a4865861251
Andrei Sabau <andrei@asabau.com>
2018-03-31 19:59:12 +0300

    eslint now checks docco guides

--- docco/image-scrapper.js
+++ docco/image-scrapper.js
@@ -1,30 +1,46 @@
 // # Image Scrapper
 
 // import the get-set-fetch dependency
-const GetSetFetch = require("get-set-fetch");
+const GetSetFetch = require('get-set-fetch');
 
 // the entire code is async for image scrapper,
 // declare an async function in order to make use of await
-async function helloWorld(){
-	// init db connection, by default in memory sqlite
-    ({ Site } = await GetSetFetch.init();
-    
-	// load site if already present, 
-	// otherwise create it by specifying a name and the first url to crawl, 
-	// only links from this location down will be subject to further crawling
-    let site = await Site.get("pexels");
-    if (!site) {
-        site = new Site('pexels', 
-        'https://www.pexels.com//index.html');
-    
-        await site.save();
-    }
-    
-	// keep crawling site until there are no more resources to crawl
-    await site.crawl();
-    
+async function imgCrawl() {
+  // init db connection, by default in memory sqlite
+  const { Site } = await GetSetFetch.init();
+
+  // load site if already present,
+  // otherwise create it by specifying a name and the first url to crawl,
+  // only links from this location down will be subject to further crawling
+  let site = await Site.get('imgSite');
+  if (!site) {
+    site = new Site(
+      'imgSite',
+      'https://imgSite/index.html',
+    );
+
+
+    // by default ExtractUrlPlugin only extracts html resources,
+    // overide the default plugin instance with a new one containing suitable options
+    site.use(new GetSetFetch.plugins.ExtractUrlPlugin({
+      extensionRe: /^(jpg|png)$/i,
+    }));
+
+    // add persistencePlugin to the current site,
+    // specify what extensions to save and where
+    site.use(new GetSetFetch.plugins.PersistResourcePlugin({
+      target: './myTargetDir',
+      extensionRe: /^(jpg|png)$/i,
+    }));
+
+    await site.save();
+  }
+
+  // keep crawling site until there are no more resources to crawl
+  await site.crawl();
+
 // end async function
 }
 
 // start crawling
-helloWorld();
+imgCrawl();
